7cb2d6b2da6bfa58457ebbaab560bfecb55465f7
Tony <pysolver33@gmail.com>
2019-10-30 01:11:18 +0800

    sample views url config

--- events/views.py
+++ events/views.py
@@ -3,8 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from events.admin import EventAdmin
+from .models import Event
 
 # Create your views here.
 
 def Hello_World(requests):
-    return HttpResponse('Hello World')
+    template = loader.get_template('admin/events/change_list.html')
+    context = {
+        'previous_month':previous_month,
+        'next_month': next_month,
+    }
+    return HttpResponse(template.render(context,requests))
